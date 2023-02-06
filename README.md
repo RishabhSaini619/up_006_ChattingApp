40eed39bb4a03240fcd0565e71443749c2cfeca6
Rishabh Saini <rishabhsaini619@gmail.com>
2023-02-06 12:44:54 +0530

    begins

---
+++ README.md
@@ -0,0 +1,7 @@
+# up_006_chattingApp
+
+Engage Chatting Application based on Flutter
+
+## Getting Started
+
+https://github.com/RishabhSaini619/up_006_chattingApp.git
---
+++ android/app/src/main/kotlin/com/flutter/udemy/practice/engage/chattingapp/up_006_chattingapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.flutter.udemy.practice.engage.chattingapp.up_006_chattingapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
